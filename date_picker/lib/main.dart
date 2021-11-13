5549f06506bc19d7c7c0614bc2b472d24903ef75
Afnan Elhussini <63553675+AfnanElhussini@users.noreply.github.com>
2021-11-13 02:27:22 +0200

    Added

---
+++ date_picker/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime date = DateTime.now();
+  datePicker() {
+    showDatePicker(
+            cancelText: 'Cancel',
+            confirmText: 'Confirm',
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2000),
+            lastDate: DateTime(2025))
+        .then((value) {
+      if (value == null) {
+        return;
+      }
+      setState(() {
+        date = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Date Picker'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          (DateFormat.yMMMEd().format(date)).toString(),
+          style: const TextStyle(fontSize: 25),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: datePicker,
+      ),
+    );
+  }
+}
+//a
